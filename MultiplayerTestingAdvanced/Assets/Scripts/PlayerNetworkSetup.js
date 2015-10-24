a04267051fe2813b39a3e2d90627030ca071a8f2
tomcamp333 <tc13270@my.bristol.ac.uk>
2015-10-24 16:14:16 +0100

    Networking additions, allows for internet games, LAN games, etc

---
+++ MultiplayerTestingAdvanced/Assets/Scripts/PlayerNetworkSetup.js
@@ -0,0 +1,17 @@
+﻿import UnityEngine.Networking;
+
+//public class PlayerNetworkSetup : NetworkBehaviour {
+	var nIdentity:NetworkIdentity;
+	var FPSCharacterCam:Camera;
+	var audioListener:AudioListener;
+	// Use this for initialization
+	function Start(){
+	if (nIdentity.isLocalPlayer){
+			GameObject.Find ("Main Camera").SetActive (false);
+			//GetComponent<CharacterController>().enabled = true;
+			GetComponent(UnityStandardAssets.Characters.FirstPerson.FirstPersonController).enabled=true;
+			FPSCharacterCam.enabled=true;
+			audioListener.enabled = true;
+		}
+	}
+//}
