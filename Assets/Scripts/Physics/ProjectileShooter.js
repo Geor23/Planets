6542ca29c57bcc5ae765cb247a2cf3c375125427
Callum Mann <cm13558@my.bristol.ac.uk>
2015-11-18 14:10:44 +0000

    Restructured folders and readded jumping/projectiles

---
+++ Assets/Scripts/Physics/ProjectileShooter.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var player:GameObject;
+var rb:Rigidbody = GetComponent.<Rigidbody>();
+
+function Start(){
+  player = GameObject.Find("Player(Clone)");
+  rb.position=player.transform.position;
+}
+
+function Update () {
+  rb.velocity=GameObject.Find("CameraPlayer").GetComponent.<Camera>().main.transform.forward*40; 
+}
