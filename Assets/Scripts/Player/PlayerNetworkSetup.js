40c2901c5f34fd137cbed67f627d3906aaa82d14
Callum Mann <cm13558@my.bristol.ac.uk>
2015-11-19 15:01:23 +0000

    fixed 2 audio listeners@

--- Assets/Scripts/Player/PlayerNetworkSetup.js
+++ Assets/Scripts/Player/PlayerNetworkSetup.js
@@ -2,15 +2,11 @@
 
 	var nIdentity:NetworkIdentity;
 	var cam1 : Camera;
-	var audioListener:AudioListener;
 	
 	
 	// Use this for initialization
 	function Start(){
 	if (nIdentity.isLocalPlayer){
-			//GameObject.Find ("Camera").SetActive (false);
-			//GetComponent("PlayerController").enabled = true;
 			cam1.enabled = true;
-			audioListener.enabled = true;
 		}
 	}
