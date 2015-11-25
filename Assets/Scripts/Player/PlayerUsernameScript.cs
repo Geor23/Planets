59fc300ec7c1db8358773628cd09dc2cf438f1c1
tomcamp333 <tc13270@my.bristol.ac.uk>
2015-11-25 13:51:45 +0000

    Added Username stuff. Stored on player and on-screen currently.

---
+++ Assets/Scripts/Player/PlayerUsernameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+public class PlayerUsernameScript : MonoBehaviour {
+	public InputField field;
+	public Text textBox;
+	public NetworkIdentity nIdentity;
+	public Text textOnScreen;
+	public Button button;
+	public void CopyText() {
+		//if (nIdentity.isLocalPlayer) {
+	    	button.gameObject.SetActive (false);
+		    field.gameObject.SetActive (false);
+			textBox.text = field.text;
+		    textOnScreen.text = field.text;
+		    textOnScreen.gameObject.SetActive (true);
+	//	}
+	}
+}
