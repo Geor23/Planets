22d2a9914edd3914b878dea79e73b3929175d6fa
James O'Connor <jamiezafar@eduroam-207-024.nomadic.bris.ac.uk>
2015-11-28 15:27:13 +0000

    Added network functionality to resource spawning

--- Assets/Scripts/Misc/ResourceSpawn.cs
+++ Assets/Scripts/Misc/ResourceSpawn.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class ResourceSpawn : MonoBehaviour {
+public class ResourceSpawn : NetworkBehaviour {
 	public Transform[] SpawnPoints;
 	public float spawnTime = 1.5f;
 	public GameObject ResourcePickUp;
@@ -9,7 +10,11 @@ public class ResourceSpawn : MonoBehaviour {
 	//public GameObject[] ResourcePickUp; in case we want to change between different resource pick up types
 
 	void Start () {
-		InvokeRepeating("SpawnResourcePickUps", spawnTime, spawnTime);
+		if(isServer){
+			InvokeRepeating("SpawnResourcePickUps", spawnTime, spawnTime);
+		} else{
+			ClientScene.RegisterPrefab((GameObject)Instantiate(ResourcePickUp, new Vector3(0,0,0), Quaternion.identity));
+		}
 	}
 	
 	void SpawnResourcePickUps(){
@@ -17,5 +22,6 @@ public class ResourceSpawn : MonoBehaviour {
 		int spawnIndex = Random.Range(0, SpawnPoints.Length);
 		objClone = (GameObject)Instantiate(ResourcePickUp, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
 		objClone.GetComponent<Rigidbody>().velocity = new Vector3(3,3,0);
+		NetworkServer.Spawn(objClone);
 	}
 }
