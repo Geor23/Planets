db521b706b06f65e14875f6dbb162982062499a2
Jamie O'Connor <jo13358@my.bristol.ac.uk>
2016-01-23 14:06:28 +0000

    made the menu button clickable

---
+++ Assets/Scripts/UI/SettingsPanelController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsPanelController : MonoBehaviour {
+
+	public GameObject settingsPanel;
+	bool isOpen = false;	
+
+	public void controlSettings(){
+		if(isOpen == false){
+			settingsPanel.SetActive(true);
+			isOpen = true;
+		}else if(isOpen == true){
+			settingsPanel.SetActive(false);
+			isOpen = false;
+		}
+		}
+
+
+}
