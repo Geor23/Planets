e5265099a04a0d1a310dbf543defa9453eb5582b
Callum Mann <cm13558@my.bristol.ac.uk>
2016-01-28 13:43:11 +0000

    now you can start from the lobby!

--- Assets/Scripts/Network/PlanetsNetworkManager.cs
+++ Assets/Scripts/Network/PlanetsNetworkManager.cs
@@ -110,9 +110,9 @@ public void SceneChange(){
 	{
 		/* This is where you can register players with teams, and spawn the player at custom points in the team space */
 		//hasConnected = true;
-		GameObject player = Instantiate (chosenCharacter, GetStartPosition ().position, Quaternion.identity) as GameObject;
+    int id = IDFromConn(conn);
+		GameObject player = Instantiate (dict[id].team==0?player1:player2, GetStartPosition ().position, Quaternion.identity) as GameObject;
 		//		GameObject player = Instantiate (playerPrefab, GetStartPosition ().position, Quaternion.identity) as GameObject;
-		
 		NetworkServer.AddPlayerForConnection (conn, player, playerControllerId);
 		
 	}
@@ -148,62 +148,7 @@ public void SceneChange(){
 	}
 	
 	public override void OnClientSceneChanged(NetworkConnection conn){
-		ClientScene.Ready(conn);
-	}
-	
-	public void OnGUI() {
-		if (!hasPickedTeam && GameObject.Find ("UsernameObject") != null) {
-			
-			if (GameObject.Find ("UsernameObject").GetComponent<PlayerUsernameScript> ().textOnScreen.IsActive ()) {
-				
-				GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
-				GUILayout.BeginHorizontal ();
-				GUILayout.FlexibleSpace ();
-				GUILayout.BeginVertical ();
-				GUILayout.FlexibleSpace ();
-				
-				if (GUILayout.Button (" Team A ")) {
-					chosenCharacter = player1 ;
-					//GameObject player = Instantiate ((GameObject)playerPrefab, GetStartPosition ().position, Quaternion.identity) as GameObject;
-					//					MeshFilter objMesh = GameObject.AddComponent<MeshFilter>();
-					//					GameObject newMesh = Resources.Load(pl1) as GameObject;
-					//					objMesh.mesh = newMesh.GetComponent<MeshFilter>().sharedMesh;
-					
-					ClientScene.AddPlayer (0);
-					//Debug.Log (GameObject.Find ("Player(Clone)"));
-					GameObject.Find ("Player(Clone)").GetComponent<TeamMember> ().RpcSetTeamID (1); 
-					hasPickedTeam = true;
-				}
-				
-				if (GUILayout.Button (" Team B ")) {
-					chosenCharacter = player2 ;
-					//						
-					//						MeshFilter objMesh = GameObject.AddComponent<MeshFilter>();
-					//						GameObject newMesh = Resources.Load(pl2) as GameObject;
-					//						objMesh.mesh = newMesh.GetComponent<MeshFilter>().sharedMesh;
-					//					//GameObject player = Instantiate ((GameObject)playerPrefab, GetStartPosition ().position, Quaternion.identity) as GameObject;
-					ClientScene.AddPlayer (0);
-					//Debug.Log (GameObject.Find ("Player(Clone)"));
-					GameObject.Find ("Player2(Clone)").GetComponent<TeamMember> ().RpcSetTeamID (2); 
-					hasPickedTeam = true;
-				}
-				
-				if (GUILayout.Button ("Auto assign")) {
-					chosenCharacter = player1 ;
-					//GameObject player = Instantiate ((GameObject)playerPrefab, GetStartPosition ().position, Quaternion.identity) as GameObject;
-					ClientScene.AddPlayer (0);
-					//Debug.Log (GameObject.Find ("Player(Clone)"));
-					GameObject.Find ("Player(Clone)").GetComponent<TeamMember> ().RpcSetTeamID (1); 
-					hasPickedTeam = true;
-				}
-				
-				GUILayout.FlexibleSpace ();
-				GUILayout.EndVertical ();
-				GUILayout.FlexibleSpace ();
-				GUILayout.EndHorizontal ();
-				GUILayout.EndArea ();
-			}
-		}
+		//ClientScene.Ready(conn);
 	}
 	
 	// called when a network error occurs
---
+++ Assets/Scripts/Network/PlayerSpawn.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+
+
+class PlayerSpawn : MonoBehaviour {
+  public void Start(){
+    ClientScene.AddPlayer(NetworkManager.singleton.client.connection, 0);
+  }
+}
