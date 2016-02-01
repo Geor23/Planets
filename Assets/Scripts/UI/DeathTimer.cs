786b174bebd02b1bf4b83fbbda58c53afe5d1868
Jamie O'Connor <jo13358@my.bristol.ac.uk>
2016-02-01 21:11:18 +0000

    Fixed bug where 2 players tried to spawn

--- Assets/Scripts/UI/DeathTimer.cs
+++ Assets/Scripts/UI/DeathTimer.cs
@@ -18,7 +18,7 @@ public class DeathTimer : MonoBehaviour {
 		if(deathTimerText.enabled == true){
 			timeLeft -= Time.deltaTime;
 			deathTimerText.text = "0:0" + Mathf.Floor(timeLeft+1).ToString();
-			if(timeLeft <= 0){
+			if(!(timeLeft > 0)){
 				deathTimerText.enabled = false;
 				deathText.enabled = false;
 				ClientScene.AddPlayer(NetworkManager.singleton.client.connection, 0);
