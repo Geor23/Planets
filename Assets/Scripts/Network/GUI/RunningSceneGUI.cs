24b7b2e32e1ecbf8eeb1cd1d2a61afa995643a57
Georgiana <apple@Apples-MacBook-Air.local>
2016-02-04 10:39:15 +0000

    added symbol next to winning team

--- Assets/Scripts/Network/GUI/RunningSceneGUI.cs
+++ Assets/Scripts/Network/GUI/RunningSceneGUI.cs
@@ -10,6 +10,8 @@ public class RunningSceneGUI : MonoBehaviour {
 	NetworkManager nm;
 	public Text teamAScore;
 	public Text teamBScore;
+	int teamA;
+	int teamB;
 	
 	public void Start() {
 
@@ -23,13 +25,24 @@ public class RunningSceneGUI : MonoBehaviour {
 		TeamScore tl = msg.ReadMessage<TeamScore>(); 
 		if (tl.team == 0) { // if we received team pirates
 
+			teamA = tl.score;
+			if (teamA > teamB ) {
+				teamAScore.text = " <quad material=1 size=20 x=0.1 y=0.1 width=0.5 height=0.5 /> ";
+			} else {
+				teamAScore.text = "";
+			}
       		//update accordingly
-			teamAScore.text = "Team Pirates: " + tl.score.ToString();
+			teamAScore.text += "Team Pirates: " + tl.score.ToString();
 
 		} else if (tl.team == 1) {  // if we received team super-corp 
-
+			teamB = tl.score;
+			if (teamA < teamB ) {
+				teamBScore.text = " <quad material=1 size=20 x=0.1 y=0.1 width=0.5 height=0.5 /> ";
+			} else {
+				teamBScore.text = "";
+			}
       		// update accordingly
-			teamBScore.text = "Team Super-Corp: " + tl.score.ToString();
+			teamBScore.text += "Team Super-Corp: " + tl.score.ToString();
 
 		} else {
 
