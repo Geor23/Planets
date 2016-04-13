fd6f8dc1b9821460fb822177c4d097c5606648cb
Callum Mann <cm13558@my.bristol.ac.uk>
2016-04-13 12:02:40 +0100

    Return ScriptBatch to its rightful place

--- Assets/Editor/Build/ScriptBatch.cs
+++ Assets/Editor/Build/ScriptBatch.cs
@@ -1,39 +1,66 @@
 using UnityEditor;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 class ScriptBatch {
 
-    static string[] playerLevels;
-    static string[] observerLevels;
-    static string[] serverLevels;
-    static string[] serverLocalClientLevels;
+    public static List<string> MandatoryScenes(){
+        string[] mandatory = {  "Assets/Scenes/LobbyScene.unity", 
+                                "Assets/Scenes/Round1.unity", 
+                                "Assets/Scenes/Round2.unity", 
+                                "Assets/Scenes/Round3.unity", 
+                                "Assets/Scenes/GameOver.unity", 
+                                "Assets/Scenes/RoundOver.unity"
+                            };
+        return new List<string>(mandatory);
+    }
     
-
-    [MenuItem("BUILD/Linux Server + Linux Client")]
+    [MenuItem("BUILD/Linux Local Server+Client")]
     public static void LinuxServerAndClient ()
     {
-        playerLevels = new string[] { "Assets/Scenes/ClientStartScene.unity", "Assets/Scenes/LobbyScene.unity", "Assets/Scenes/RunningScene.unity"};
-        observerLevels = new string[] { "Assets/Scenes/ObserverStartScene.unity", "Assets/Scenes/LobbyScene.unity", "Assets/Scenes/RunningScene.unity"};
-        serverLevels = new string[] { "Assets/Scenes/ServerStartScene.unity", "Assets/Scenes/LobbyScene.unity", "Assets/Scenes/RunningScene.unity"};
-        serverLocalClientLevels = new string[] { "Assets/Scenes/ServerLocalClientScne.unity", "Assets/Scenes/LobbyScene.unity", "Assets/Scenes/RunningScene.unity"};
-        // Get filename.
-        string path = EditorUtility.SaveFolderPanel("Chooce location to build in", "", "");
-        // Build player.
-        BuildPipeline.BuildPlayer(playerLevels, path + "/playerClient.exe", BuildTarget.StandaloneLinux, BuildOptions.Development);
-        //BuildPipeline.BuildPlayer(serverLevels, path + "/serverClient.exe", BuildTarget.StandaloneLinux, BuildOptions.Development);
-        BuildPipeline.BuildPlayer(serverLocalClientLevels, path + "/serverLocalClient.exe", BuildTarget.StandaloneLinux, BuildOptions.Development);
+        List<string> serverLocalClientLevels = MandatoryScenes(); 
+        serverLocalClientLevels.Insert(0, "Assets/Scenes/ServerLocalClientScne.unity");
+        string path = EditorUtility.SaveFolderPanel("Choose location to build in", "", "");
+        BuildPipeline.BuildPlayer(serverLocalClientLevels.ToArray(), path + "/serverLocalClient.exe", BuildTarget.StandaloneLinux64, BuildOptions.Development);
 
         // Copy a file from the project folder to the build folder, alongside the built game.
         //FileUtil.CopyFileOrDirectory("Assets/Scenes/WebPlayerTemplates/Readme.txt", path + "Readme.txt");
     }
 
-    [MenuItem("BUILD/Windows Server + Windows Client")]
+    [MenuItem("BUILD/Windows Local Server+Client")]
     public static void WindowsServerAndClient (){
+        List<string> serverLocalClientLevels = MandatoryScenes(); 
+        serverLocalClientLevels.Insert(0, "Assets/Scenes/ServerLocalClientScne.unity");
+        string path = EditorUtility.SaveFolderPanel("Choose location to build in", "", "");
+        BuildPipeline.BuildPlayer(serverLocalClientLevels.ToArray(), path + "/serverLocalClient.exe", BuildTarget.StandaloneWindows64, BuildOptions.Development);
 
     }
+
+    [MenuItem("BUILD/OSX Local Server+Client")]
+    public static void OSXServerAndClient (){
+        List<string> serverLocalClientLevels = MandatoryScenes(); 
+        serverLocalClientLevels.Insert(0, "Assets/Scenes/ServerLocalClientScne.unity");
+        string path = EditorUtility.SaveFolderPanel("Choose location to build in", "", "");
+        BuildPipeline.BuildPlayer(serverLocalClientLevels.ToArray(), path + "/serverLocalClient.exe", BuildTarget.StandaloneOSXIntel64, BuildOptions.Development);
+
+    }
+
     [MenuItem("BUILD/Deployment build (Linux Server; Android, iOS clients; Windows Observer client)")]
     public static void DeploymentServerClients (){
-        UnityEngine.Debug.Log("Wat");
+        //For Android
+        List<string> playerLevels = MandatoryScenes();
+        playerLevels.Insert(0, "Assets/Scenes/ClientStartScene.unity"); 
+        List<string> observerLevels = MandatoryScenes();
+        observerLevels.Insert(0, "Assets/Scenes/ObserverStartScene.unity"); 
+        List<string> serverLevels = MandatoryScenes();
+        serverLevels.Insert(0, "Assets/Scenes/ServerStartScene.unity"); 
+        string path = EditorUtility.SaveFolderPanel("Choose location to build in", "", "");
+
+        EditorUserBuildSettings.androidBuildSubtarget = MobileTextureSubtarget.ASTC;
+        //BuildPipeline.BuildPlayer(playerLevels.ToArray(), path + "/planets-player.apk", BuildTarget.Android, BuildOptions.Development);
+        BuildPipeline.BuildPlayer(serverLevels.ToArray(), path + "/planets-server.exe", BuildTarget.StandaloneLinux64, BuildOptions.Development);
+        BuildPipeline.BuildPlayer(observerLevels.ToArray(), path + "/planets-observer.exe", BuildTarget.StandaloneWindows64, BuildOptions.Development);
     }
 }
