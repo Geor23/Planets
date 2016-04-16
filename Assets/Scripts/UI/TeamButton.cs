05d5d84ba76be09e256791505998d05595364812
Tom Michiels <tmichiels@gmail.com>
2016-04-16 19:42:38 +0100

    Clicks don't seem to be working - unsure if it's Ubuntunity playing up

---
+++ Assets/Scripts/UI/TeamButton.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TeamButton : MonoBehaviour {
+
+public bool pirate = false;
+public bool superCorp = false;
+public LobbyGUI lobbyGUI;
+public Text header;
+public Text otherHeader;
+
+	void Start () {
+		header.color = new Color(1F,0.5F,0.5F,1F);
+		Debug.Log (GetComponent<RectTransform>().rect.size);
+	}
+
+	void OnGUI () {
+		Debug.Log ("OnGUI called");
+		if (Event.current.type == EventType.MouseUp && GetComponent<RectTransform>().rect.Contains(Event.current.mousePosition))
+		{
+			Debug.Log("Clicked: ");
+			if (pirate) { 
+				Debug.Log("Pirate Team");
+				lobbyGUI.ChooseTeamPirates();
+			} else { 
+				Debug.Log("SuperCorp Team");
+				lobbyGUI.ChooseTeamSuperCorp();
+			}
+			header.color = new Color (1F,1F,1F,1F);
+			otherHeader.color = new Color (1F,0.5F,0.5F,1F);
+		}
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0) && GetComponent<RectTransform>().rect.Contains(Input.mousePosition)) {
+			Debug.Log("Clicked: ");
+			if (pirate) { 
+				Debug.Log("Pirate Team");
+				lobbyGUI.ChooseTeamPirates();
+			} else { 
+				Debug.Log("SuperCorp Team");
+				lobbyGUI.ChooseTeamSuperCorp();
+			}
+			header.color = new Color (1F,1F,1F,1F);
+			otherHeader.color = new Color (1F,0.5F,0.5F,1F);
+		}
+	}
+
+	public void buttonEffect () {
+		Debug.Log("Clicked: ");
+		if (pirate) { 
+			Debug.Log("Pirate Team");
+			lobbyGUI.ChooseTeamPirates();
+		} else { 
+			Debug.Log("SuperCorp Team");
+			lobbyGUI.ChooseTeamSuperCorp();
+		}
+		header.color = new Color (1F,1F,1F,1F);
+		otherHeader.color = new Color (1F,0.5F,0.5F,1F);
+	}
+
+}
