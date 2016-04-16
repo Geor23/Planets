cc98bb12958be7bd60052650c7478283278a1a8d
Callum Mann <cm13558@my.bristol.ac.uk>
2016-04-16 17:26:46 +0100

    Added controls to new camera

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -8,13 +8,43 @@ public class CameraController : MonoBehaviour {
     private int count;
     private Vector3 point;
     private Vector3 avgUp;
+
     // public GameObject cam;
 
     public GameObject[] playersS;
     public GameObject[] playersP;
     public float speed = 0.1F;
+    private Vector3 pos;
+
+    void Start(){
+        transform.position = new Vector3(0,0,0);
+        pos = new Vector3(0, 0, 0);
+    }
 
     void Update () {
+
+        if (Input.GetKey(KeyCode.A)) {  //Camera one commands
+            pos -= transform.right.normalized/10;
+        }else if(Input.GetKey(KeyCode.D)){
+            pos += transform.right.normalized / 10;
+        }
+
+        if(Input.GetKey(KeyCode.W)) {
+            pos -= transform.up.normalized / 10;
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            pos += transform.up.normalized / 10;
+        }
+
+        if(Input.GetKey(KeyCode.Q)) {
+            pos += transform.forward.normalized / 10;
+        }else if (Input.GetKey(KeyCode.E)) {
+            pos -= transform.forward.normalized / 10;
+        }
+
+
+        transform.position = pos;
+
         count = 0;
         playersS = GameObject.FindGameObjectsWithTag("PlayerSuperCorp");
         playersP = GameObject.FindGameObjectsWithTag("PlayerPirate");
@@ -36,7 +66,6 @@ public class CameraController : MonoBehaviour {
 
         point = sum/count;
         avgUp = camUps/count;
-        transform.position = new Vector3(0,0,0);
         transform.LookAt(point, -avgUp);
     }
 }
