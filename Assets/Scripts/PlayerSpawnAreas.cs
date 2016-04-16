4bea8d6a296a1fa6401415b751c6e10763848dba
EleftheriosC <celeftherios@hotmail.com>
2016-04-16 14:30:36 +0100

    Added objects as reference points for the players to spawn

--- Assets/Scripts/PlayerSpawnAreas.cs
+++ Assets/Scripts/PlayerSpawnAreas.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerSpawnAreas : MonoBehaviour {
 
     Vector3 offset;
+    public GameObject spawnObject1;
+    public GameObject spawnObject2;
+
 
     //Generate spawn points for both teams. The spawn point of each team should be
     //relative to the spawn point of the other
@@ -18,7 +21,11 @@ public class PlayerSpawnAreas : MonoBehaviour {
         offset.y += Random.Range(-radius, radius);
         offset.z += Random.Range(-radius, radius);
 
-        spawnPoint = Random.insideUnitCircle * 5;
+        //spawnPoint = Random.insideUnitCircle * 5;
+        spawnPoint.x = spawnObject1.transform.position.x + Random.Range(-1, 1);
+        spawnPoint.y = spawnObject1.transform.position.x + Random.Range(-1, 1);
+        spawnPoint.z = spawnObject1.transform.position.x + Random.Range(-1, 1);
+
 
         if (team == 0) {
             return spawnPoint;
