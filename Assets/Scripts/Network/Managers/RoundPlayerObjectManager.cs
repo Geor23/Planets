521a70bbbb06d6a9ff01ac37fb76a009dc742cf7
tomcamp333 <tc13270@my.bristol.ac.uk>
2016-04-18 11:46:27 +0100

    Added new classes for round management

---
+++ Assets/Scripts/Network/Managers/RoundPlayerObjectManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Spawn+Death manager maybe?
+//Use to manage player objects on the local side, with functionality to request server to 
+public class RoundPlayerObjectManager { //TODO
+
+    private List<int> deadPlayers;
+    private List<KillHistory> killHistory;
+
+    public RoundPlayerObjectManager(){
+        deadPlayers = new List<int>();
+        killHistory = new List<KillHistory>();
+    }
+
+    //Spawns player with the supplied Id. Currently done on server, maybe do on Observer if we remove networked objects
+    public void spawnPlayerServer(int playerId){
+
+    }
+
+    //Kills player, potentially starts timer for respawn too
+    public void killPlayerLocal(int playerIdKilled, int playerIdKiller){ //TODO
+        //Remove game object locally
+        //Remove score (call below stuff)
+    }
+
+    //Kills player, potentially starts timer for respawn too
+    public void killPlayerServer(int playerId, int playerIdKiller){ //TODO
+        //Remove game object locally
+        //Remove score (call below stuff)
+    }
+
+    public void relayDataToServer(){
+        //ToDO
+    }
+}
---
+++ Assets/Scripts/Network/Managers/RoundScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoundScoreManager : MonoBehaviour {
+    //Deals with scores of players. Scoring/loss of score by death (overlap with above). Potentially stores individual scores as they'll pass through anyway.
+    //Deals with both Observers sending scores, and the Server passing scores to clients/recieving and storing them.
+        int pirateScore;
+        int superScore;
+        //List of players/their scores potentially
+        private Dictionary<int, int> playerScores;
+
+        public RoundScoreManager(){
+            playerScores = new Dictionary<int, int>();
+        }
+
+
+        public void increasePlayerScore(int playerId, int score){
+            //TODO
+        }
+
+        public void decreasePlayerScore(int playerId, int score){
+            //TODO
+        }
+
+        //Request to add/remove score
+        public void ChangeScoreServer(int playerId, int scoreChange){ //TODO
+
+        }
+
+        //Returns score when requested
+        public int sendScoreClient(){ //TODO
+            return 0; //Will return score
+        }
+
+        public void relayDataToServer(){
+            //TODO
+        }
+}
