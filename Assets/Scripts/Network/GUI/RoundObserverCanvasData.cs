3a4266a13be3725f787b244957cdf616d6a0d4ca
Callum Mann <cm13558@my.bristol.ac.uk>
2016-04-20 09:39:50 +0100

    Scores now showing on observer screen

--- Assets/Scripts/Network/GUI/RoundObserverCanvasData.cs
+++ Assets/Scripts/Network/GUI/RoundObserverCanvasData.cs
@@ -7,16 +7,36 @@ public class RoundObserverCanvasData : MonoBehaviour {
 
 	public Text latestKill;
 	public Text killFeed;
+	public Text pirateScore;
+	public Text superCorpScore;
+	public GameObject piratesWinning;
+	public GameObject superCorpWinning;
 	public RoundEvents re;
 
 
 	void Start(){
 		re = GameObject.Find("RoundEvents").GetComponent<RoundEvents>();
+		if(re == null) {
+			Debug.LogError("Couldn't find RoundEvents!");
+		}
 		InvokeRepeating("UpdateKills", 0, 1);
 	}
 
 	void UpdateKills(){
+		int pirateScoreInt = re.getRoundScoreManager().getPirateScore();
+		int superCorpScoreInt = re.getRoundScoreManager().getSuperCorpScore();
 		latestKill.text = re.getRoundPlayerObjectManager().getLatestKill();
 		killFeed.text = re.getRoundPlayerObjectManager().getKillsAsList(5); // Show last 5 kills;
+		pirateScore.text = pirateScoreInt.ToString();
+		superCorpScore.text = superCorpScoreInt.ToString();
+
+		if(pirateScoreInt > superCorpScoreInt){
+			piratesWinning.SetActive(true);
+			superCorpWinning.SetActive(false);
+		}
+		if(pirateScoreInt < superCorpScoreInt){
+			piratesWinning.SetActive(false);
+			superCorpWinning.SetActive(true);	
+		}
 	}
 }
--- Assets/Scripts/Network/Managers/RoundScoreManager.cs
+++ Assets/Scripts/Network/Managers/RoundScoreManager.cs
@@ -6,11 +6,13 @@ public class RoundScoreManager {
     //Deals with scores of players. Scoring/loss of score by death (overlap with above). Potentially stores individual scores as they'll pass through anyway.
     //Deals with both Observers sending scores, and the Server passing scores to clients/recieving and storing them.
     private int pirateScore;
-    private int superCorpScore;
+    private int superCorpScore; 
     private PlayerManager pm;
     private Dictionary<int, Player> playerDict;
 
     public RoundScoreManager(Dictionary<int, Player> pd){
+        pirateScore = 0;
+        superCorpScore = 0;
         pm = PlayerManager.singleton;
         playerDict = pd; //Reference to other dictionary, means changes here change there too
 
