a4ae3c77a31fab188d3f50f1712c6f1d0a7946fa
Callum Mann <cm13558@my.bristol.ac.uk>
2016-04-22 21:38:58 +0100

    Now enabling the dome ocntroller will switch to dome cams!

--- Assets/Scripts/Planetarium/PlanetariumDomeControl.cs
+++ Assets/Scripts/Planetarium/PlanetariumDomeControl.cs
@@ -2,7 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class PlanetariumDomeControl : MonoBehaviour {
-    void Awake() {
+    void Start() {
         DontDestroyOnLoad(transform.gameObject);
+        turnOffOtherCameras();
     }
+
+    void turnOffOtherCameras(){
+    	GameObject[] cams = GameObject.FindGameObjectsWithTag("MainCamera");
+    	foreach(GameObject cam in cams){
+    		cam.SetActive(false);
+    	}
+    }
+
+    void OnLevelWasLoaded(int level) {
+        turnOffOtherCameras();
+    }
+
 }
