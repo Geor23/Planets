cad96a1acd194b420ab9062632614876105b25ea
Georgiana <ig13289@my.bristol.ac.uk>
2016-04-26 20:47:02 +0100

    trying to fix shield script

--- Assets/Scripts/Network/GUI/GameOverGUI.cs
+++ Assets/Scripts/Network/GUI/GameOverGUI.cs
@@ -12,6 +12,12 @@ public class GameOverGUI : MonoBehaviour {
 	public Text teamS;
   public Text ScoreP;
   public Text ScoreS;
+  public GameObject tumor-pirate;
+  public GameObject tumor-supercorp;
+  public GameObject maze-pirate;
+  public GameObject maze-supercorp;
+  public GameObject city-pirate;
+  public GameObject city-supercorp;
 
   public void Start() {
 
@@ -40,14 +46,35 @@ public class GameOverGUI : MonoBehaviour {
     int pirateCounter = 0;
     int superCorpCounter = 0;
 
-    if (tl.round1P >= tl.round1S) pirateCounter ++;
-    else superCorpCounter ++;
+    if (tl.round1P >= tl.round1S) {
+      pirateCounter ++;
+      tumor-pirate.SetActive(true);
+      tumor-supercorp.SetActive(false);
+    } else {
+      superCorpCounter ++;
+      tumor-pirate.SetActive(false);
+      tumor-supercorp.SetActive(true);
+    }
 
-    if (tl.round2P >= tl.round2S) pirateCounter ++;
-    else superCorpCounter ++;
+    if (tl.round2P >= tl.round2S) {
+      pirateCounter ++;
+      maze-pirate.SetActive(true);
+      maze-supercorp.SetActive(false);
+    } else {
+      superCorpCounter ++;
+      maze-pirate.SetActive(false);
+      maze-supercorp.SetActive(true);
+    }
 
-    if (tl.round3P >= tl.round3S) pirateCounter ++;
-    else superCorpCounter ++;
+    if (tl.round3P >= tl.round3S) {
+      pirateCounter ++;
+      city-pirate.SetActive(true);
+      city-supercorp.SetActive(false);
+    } else {
+      superCorpCounter ++;
+      city-pirate.SetActive(false);
+      city-supercorp.SetActive(true);
+    }
 
     ScoreP.text = pirateCounter.ToString();
     ScoreS.text = superCorpCounter.ToString();
