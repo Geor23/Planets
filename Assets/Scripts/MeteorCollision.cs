67dfec7a9bb40d35c162a25517934ca9fa8e8ecc
EleftheriosC <celeftherios@hotmail.com>
2016-05-09 23:44:59 +0100

    Created a script for when meteor collide with the planet the meteors get destroyed

---
+++ Assets/Scripts/MeteorCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorCollision : MonoBehaviour {
+
+    private ResourcePowerUpManager resourcePowerUpManager;
+
+    // Use this for initialization
+    void Start () {
+        resourcePowerUpManager = GameObject.FindGameObjectWithTag("Planet").GetComponent<ResourcePowerUpManager>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Planet"))
+        {
+            resourcePowerUpManager.meteorCollision(gameObject);
+        }
+    }
+    }
